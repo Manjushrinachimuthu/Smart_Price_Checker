60f4c2bf3ad6c5da7b364e54577807a548c4b3a3
YOUR_GITHUB_USERNAME <manjushrithurai@gmail.com>
2026-04-12 21:09:35 +0530

    Delete all files

--- pricebackend/src/main/java/com/manju/pricebackend/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manju.pricebackend.repository;
-
-import com.manju.pricebackend.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
